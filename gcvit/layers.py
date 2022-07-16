cfc004a26bb32c67c99de75daf2df34ebeffc8ed
Awsaf <awsaf49@gmail.com>
2022-07-17 03:41:57 +0600

    `PatchEmbedding` added

--- gcvit/layers.py
+++ gcvit/layers.py
@@ -110,3 +110,36 @@ class ReduceSize(tf.keras.layers.Layer):
             "keep_dim":self.keep_dim,
         })
         return config
+
+class PatchEmbed(nn.Module):
+    def __init__(self, in_chans=3, dim=96):
+        super().__init__()
+        self.proj = nn.Conv2d(in_chans, dim, 3, 2, 1)
+        self.conv_down = ReduceSize(dim=dim, keep_dim=True)
+
+    def forward(self, x):
+        x = self.proj(x).permute(0, 2, 3, 1)
+        x = self.conv_down(x)
+        return x
+
+@tf.keras.utils.register_keras_serializable(package="gcvit")
+class PatchEmbedding(tf.keras.layers.Layer):
+    def __init__(self, dim, **kwargs):
+        super().__init__(**kwargs)
+        self.dim = dim
+
+    def build(self, input_shape):
+        self.pad = tf.keras.layers.ZeroPadding2D(1)
+        self.proj = tf.keras.layers.Conv2D(self.dim, kernel_size=3, strides=2, name='proj')
+        self.conv_down = ReduceSize(keep_dim=True, name='conv_down')
+        super().build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        x = self.proj(self.pad(inputs))
+        x = self.conv_down(x)
+        return x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'dim': self.dim})
+        return config
