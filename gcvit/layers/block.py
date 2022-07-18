20aff872566ea6f336bdd222b6732537e856bbbc
Awsaf <awsaf49@gmail.com>
2022-07-18 19:50:11 +0600

    update for `global_query`

--- gcvit/layers/block.py
+++ gcvit/layers/block.py
@@ -8,12 +8,12 @@ from .feature import Mlp
 
 @register_keras_serializable(package="gcvit")
 class GCViTBlock(tf.keras.layers.Layer):
-    def __init__(self, window_size, num_heads, mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0.,
-                 attn_drop=0., path_drop=0., act_layer='gelu', attention='global',
-                 layer_scale=None, **kwargs):
+    def __init__(self, window_size, num_heads, global_query, mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0.,
+                 attn_drop=0., path_drop=0., act_layer='gelu', layer_scale=None, **kwargs):
         super().__init__(**kwargs)
         self.window_size = window_size
         self.num_heads = num_heads
+        self.global_query = global_query
         self.mlp_ratio = mlp_ratio
         self.qkv_bias = qkv_bias
         self.qk_scale = qk_scale
@@ -21,15 +21,17 @@ class GCViTBlock(tf.keras.layers.Layer):
         self.attn_drop = attn_drop
         self.path_drop = path_drop
         self.act_layer = act_layer
-        self.attention = attention
         self.layer_scale = layer_scale
 
     def build(self, input_shape):
-        B, H, W, C = input_shape[0]
+        if self.global_query:
+            B, H, W, C = input_shape[0]
+        else:
+            B, H, W, C = input_shape
         self.norm1 = tf.keras.layers.LayerNormalization(axis=-1, epsilon=1e-05, name='norm1')
-        self.attn_layer = WindowAttentionGlobal if self.attention == 'global' else WindowAttention
-        self.attn = self.attn_layer(window_size=self.window_size, 
+        self.attn = WindowAttention(window_size=self.window_size, 
                                    num_heads=self.num_heads,
+                                   global_query=self.global_query,
                                    qkv_bias=self.qkv_bias, 
                                    qk_scale=self.qk_scale, 
                                    attn_dropout=self.attn_drop, 
@@ -58,15 +60,19 @@ class GCViTBlock(tf.keras.layers.Layer):
         super().build(input_shape)
 
     def call(self, inputs, **kwargs):
-        inputs,  query = inputs
+        if self.global_query:
+            inputs, q_global = inputs
         B, H, W, C = tf.shape(inputs)
         x = self.norm1(inputs)
-        # window partition
-        x = window_partition(x, self.window_size)
+        # create windows and concat them in batch axis
+        x = window_partition(x, self.window_size)  # (B_, win_h, win_w, C)
         # flatten patch
-        x = tf.reshape(x, shape=[-1, self.window_size * self.window_size, C])
+        x = tf.reshape(x, shape=[-1, self.window_size * self.window_size, C])  # (B_, N, C) => (batch*num_win, num_token, feature)
         # attention
-        x = self.attn([x, query])
+        if self.global_query:
+            x = self.attn([x, q_global])
+        else:
+            x = self.attn(x)
         # reverse window partition
         x = window_reverse(x, self.window_size, H, W, C)
         # FFN
@@ -79,6 +85,7 @@ class GCViTBlock(tf.keras.layers.Layer):
         config.update({
             'window_size': self.window_size,
             'num_heads': self.num_heads,
+            'global_query': self.global_query,
             'mlp_ratio': self.mlp_ratio,
             'qkv_bias': self.qkv_bias,
             'qk_scale': self.qk_scale,
@@ -86,7 +93,6 @@ class GCViTBlock(tf.keras.layers.Layer):
             'attn_drop': self.attn_drop,
             'path_drop': self.path_drop,
             'act_layer': self.act_layer,
-            'attention': self.attention,
             'layer_scale': self.layer_scale,
             'num_windows': self.num_windows,
         })
