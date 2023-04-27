66cb0cf414ab849af9a79023ad1ab3316bdfc80d
André Pedersen <andrped94@gmail.com>
2023-04-27 14:59:14 +0200

    Fixed typo in reqs

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[
-        "tensorflow-addons"
+        "tensorflow-addons",
         "numpy",
         "matplotlib",
     ],
